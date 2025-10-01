f5925ce4c42cd5ae3ae23419bf128bcd53c7e703
Ricardo <riserap92@gmail.com>
2025-10-01 12:08:25 -0300

    refactor: simplify TypeORM configuration by using DATABASE_URL

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,7 @@ export const getTypeOrmConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: configService.get('DATABASE_HOST'),
-  port: parseInt(configService.get('DATABASE_PORT') || '5432'),
-  username: configService.get('DATABASE_USERNAME'),
-  password: configService.get('DATABASE_PASSWORD'),
-  database: configService.get('DATABASE_NAME'),
+  url: configService.get('DATABASE_URL'),
   schema: 'web_app',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
